3dfa706e901bebd4dd2404154ec1d3246b5fc2dc
kwiby <134721335+kwiby@users.noreply.github.com>
2025-06-16 00:32:18 -0400

    sorry for multiple small commits lol, keep forgetting stuff

--- GameController.java
+++ GameController.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
